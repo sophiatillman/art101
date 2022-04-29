6f81fa1b7845800cb65358437244853bc55fef5e
sophiatillman <102751656+sophiatillman@users.noreply.github.com>
2022-04-29 13:46:35 -0700

    test 3

---
+++ lab6/js/lab.js
@@ -0,0 +1,25 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @AuthorName   Sophia
+ * @created 11.05.2009
+ * @License  Public Domain
+ */
+ // Define Variables
+ myTransport = ["Toyota Prius","Bike","Skateboard", "RidesfromFriends", "SlipnSlide"];
+ myRide = {
+    make: "Toyota",
+    model: "Prius",
+    color: "Red",
+    year: "2017",
+    age: function() {
+        return 2022 - age;
+    }
+}
+
+// Output
+document.writeln("Kinds of Transport I use," "myTransport" "<br/>");
+document.writeln("MyMainRide: <pre>"
+ JSON.stringify(myRide, null, '/t'), "</pre>");
