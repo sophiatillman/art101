a7ad4a12d59ed7fd470c02beaae465c1fa391eb2
sophiatillman <102751656+sophiatillman@users.noreply.github.com>
2022-05-02 19:52:32 -0700

    fixed

---
+++ lab4/js/lab.js
@@ -0,0 +1,25 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
+//print the board before each new game
+ //print 7x6 grid
+//accept user input for color of game piece
+ //user input → select ‘Yellow’ or ‘Red’ game piece
+//accept user input for ‘Player 1 move’
+//user input → Player 1 select a column 1-7 to place selected color piece
+//validate user’s move
+//choose computer move
+ //computer select a column 1-7 to place selected color piece
+//Loop(or Repeat) user input for ‘Player 1 move’ and computer move until either player achieves 4 game pieces in a row
+ //Can be diagonal (- or + slope)
+ //Can be vertical
+//Can be horizontal
