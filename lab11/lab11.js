0ef4ba966977a4802d96453f8999d5879c8d93b0
sophiatillman <102751656+sophiatillman@users.noreply.github.com>
2022-05-16 10:25:57 -0700

    lab12

--- lab11/lab11.js
+++ lab11/lab11.js
@@ -10,18 +10,19 @@
  // sortUserName - a function that takes user input and sorts the letters
  // of their Name
  // add button below Challenges div
+ // add button below Challenges div
  var butt1 = $("#challenges").append("<button> Press Me! </button>");
  //add click and toggle
- $("button").click(function(){
+ $(butt1).click(function(){
    $("#p1").toggleClass("selected");
  });
  // add a button below the problems div
  var butt2 = $("#problems").append("<button> This is a button! </button>");
- $("button").click(function(){
+ $(butt2).click(function(){
    $("#p2").toggleClass("selected2");
  });
  // add a button below the results div
  var butt3 = $("#results").append("<button> Fun with buttons! </button>");
- $("button").click(function(){
+ $(butt3).click(function(){
    $("#p3").toggleClass("selected3");
  });
