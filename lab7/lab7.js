952ed0464a4b582eb2f333593168217dc146932c
sophiatillman <102751656+sophiatillman@users.noreply.github.com>
2022-05-27 23:35:36 -0700

    w

--- lab7/lab7.js
+++ lab7/lab7.js
@@ -7,27 +7,21 @@
  * @created 11.05.2009
  * @License  Public Domain
  */
- //
- 
-// sortUserName - a function that takes user input and sorts the letters
-// of their name
 function sortUserName() {
-	var userName = window.prompt("Type your name in the box now!");
-	console.log("userName =", userName);
-	// split string to array
-	var nameArray = userName.split('');
-	console.log("nameArray =", nameArray);
-	// sort the array
-	var nameArraySort = nameArray.sort();
-	console.log("nameArraySort =", nameArraySort);
-	// join array back to string
-	var nameSorted = nameArraySort.join('');
-	console.log("nameSorted =", nameSorted);
-	// Note that I could have done the above lines as a single line:
-	//	userName.toLower().split("").sort().join("")
-	return nameSorted;
+  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+  console.log("userName =", userName);
+  //split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  //sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  //join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
 }
 
-// output
+//Output
 document.writeln("Here's your new name: ",
-	sortUserName(), "</br");
+  sortUserName(), "</br>");
