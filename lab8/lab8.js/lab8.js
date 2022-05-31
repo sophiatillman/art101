a0853e9883069e001924f25c0ec43bbed46fffe6
sophiatillman <102751656+sophiatillman@users.noreply.github.com>
2022-05-30 17:18:35 -0700

    Update lab8.js

--- lab8/lab8.js/lab8.js
+++ lab8/lab8.js/lab8.js
@@ -6,42 +6,25 @@
  * @AuthorName   Sophia
  * @created 11.05.2009
  * @License  Public Domain
- */
+ **/
+  function isEven(x){
+  }
+ function isEven(x){
+     return (x % 2 == 0);
+ }
+ //test function
+ console.log("is 1 even? ", isEven(1));
+ console.log("is 2 even? ", isEven(2));
 
-function firstThing(test) {
-    console.log(test + ": This is the first thing.<br>")
-}
+ array = [200, 5, 70, 134, 789, 1000, 600]
+ console.log("my array", array);
 
-function secondThing(test) {
-    console.log(test + ": This is the second thing.<br>")
-}
+ var result = array.map(isEven);
+ //should return [true, false, true, true, false, true, true]
+ console.log("test of evenness of array",result);
 
-function thirdThing(test) {
-    console.log(test + ": This is the third thing.<br>")
-}
-
-firstThing("TEST 1");
-secondThing("TEST 1");
-thirdThing("TEST 1");
-
-// test 2 - in order
-setTimeout(function() {
-    firstThing("TEST 2");
-}, 0);
-setTimeout(function() {
-    secondThing("TEST 2");
-}, 0);
-setTimeout(function() {
-    thirdThing("TEST 2");
-}, 0);
-
-// test 3 - order is 2, 3, 1.
-setTimeout(function() {
-    firstThing("TEST 3");
-}, 3000);
-setTimeout(function() {
-    secondThing("TEST 3");
-}, 1000);
-setTimeout(function() {
-    thirdThing("TEST 3");
-}, 2000);
+ var result = array.map(function(x){
+   return x ** 0.2;
+ })
+ //should return [2.8853998118144273, 1.379729661461215, 2.3389428374280206, 2.6633053388644834, 3.7967797155355707, 3.9810717055349727, 3.5944318187380233]
+ console.log("new array!",result);
