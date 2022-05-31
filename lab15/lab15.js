74d3067eb1a33ad4311fdaea0e5e5b7cca061a64
sophiatillman <102751656+sophiatillman@users.noreply.github.com>
2022-05-30 21:33:29 -0700

    2

--- lab15/lab15.js
+++ lab15/lab15.js
@@ -8,6 +8,35 @@
 *Created: 19 May
 *License:Public Domain
 */
+if (window.navigator.geolocation) {
+ // Geolocation available
+}
+window.navigator.geolocation
+  .getCurrentPosition(console.log, console.log);
+getCurrentPosition(successCallback, failureCallback);
+{
+  coords = {
+    latitude: 51.4554803,
+    longitude: -2.0843058,
+    altitude: null,
+    accuracy: 49,
+    altitudeAccuracy: null,
+    heading: null,
+    speed: null,
+  },
+  timestamp: 1583860898576
+}
+const successfulLookup = position => {
+  const { latitude, longitude } = position.coords;
+  fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=1234`)
+    .then(response => response.json();
+    .then(console.log); // Or do whatever you want with the result
+    window.navigator.geolocation
+      .getCurrentPosition(successfulLookup, console.log);
+      results[0].formatted; // Gives something like '54, My street, My Town, My Post Code, My Country
+
+
+
 
 const myElement = document.getElementById("demo");
 
